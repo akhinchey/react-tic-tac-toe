a9298588bd8504c290a81ddc4485764c9ab98ebb
Amanda Hinchey <akhinchey@gmail.com>
2017-04-08 15:03:37 -0400

    Add winner declaration.

--- src/Board.js
+++ src/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
@@ -19,12 +19,21 @@ class Board extends React.Component {
     });
   }
 
+
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
+
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      };
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -49,3 +58,23 @@ class Board extends React.Component {
 }
 
 export default Board;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
